be7df6f92e131b4084c3529e08887df79ff64307
Baxtiyor <oqonboyevb@gmail.com>
2021-11-15 12:49:09 +0500

    class

---
+++ index.js
@@ -0,0 +1,29 @@
+let user = { name: "John", age: 30 };
+
+console.log( "age" in user ); 
+console.log( "best" in user );
+class User {
+
+    constructor(name) {
+      // invokes the setter
+      this.name = name;
+    }
+  
+    get name() {
+      return this._name;
+    }
+  
+    set name(value) {
+      if (value.length < 4) {
+        alert("Name is too short.");
+        return;
+      }
+      this._name = value;
+    }
+  
+  }
+  
+  let user = new User("John");
+  alert(user.name); // John
+  
+  user = new User(""); // Name is too short
